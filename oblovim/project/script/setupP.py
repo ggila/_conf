7261fbb37c5f220028b78ac26e017b2ec372252b
Gautier <gilabert.gautier@gmail.com>
2015-11-30 13:52:23 +0100

    dodo

--- oblovim/project/script/setupP.py
+++ oblovim/project/script/setupP.py
@@ -10,7 +10,7 @@ if len(sys.argv) != 2:
     exit(0)
 
 pName = sys.argv[1]
-includePattern = re.compile('\n#include [<"]([^">]+)[">]\n')
+includePattern = re.compile('#include ([<"])([^">]+)[">]')
 
 print pName
 
@@ -37,6 +37,9 @@ class Project:
     - pwd
     - listfile
     - src
+    - self.name = pName
+    - self.src = []
+    - self.inc = []
     """
 
 
@@ -44,15 +47,23 @@ class Project:
 #    def setUpDir(self):
 #        os.system('cp -r  ~/config/oblovim/project/template/newP .')
 
-    def checkInclude(self, l):
-        for e in l: print e
-
     def readCFile(self, path):
+        """
+                find include header with:
+                re.compile('#include ([<"])([^">]+)[">]')
+                add to self.inc sets
+        """
         with open(path, 'r') as f:
             text = "\n{0}".format(f.read())
-            self.checkInclude(re.findall(includePattern, text))
+            cFileInc = re.findall(includePattern, text)
+            self.incLocal |= set([b for a, b in cFileInc if a == '"'])
+            self.incSys |= set([b for a, b in cFileInc if a == '<'])
+
 
     def checkSource(self, rep):
+        """
+                travel through ./src/ and update attributes
+        """
         for f in os.listdir(rep):
             if not os.path.isdir(rep + '/' + f) and f[0] != '.' and f[-2:] == '.c':
                 self.readCFile(rep + "/" + f)
@@ -60,19 +71,46 @@ class Project:
             if os.path.isdir(rep + '/' + f) and f[0] != '.':
                 self.checkSource(rep + "/" + f)
 
-    def Update(self):
+    def makeHeader(self):
+        """
+                generate project header
+        """
+        with open("./inc/" + self.name + ".h", "w") as f:
+            f.write("#ifndef {0}_H\n".format(self.pName.upper())
+            f.write("# define {0}_H\n\n".format(self.pName.upper()))
+            for elem in this.incSys:
+                f.write('#include <{0}>\n'.format(elem))
+            f.write("\n")
+            for elem in this.incLocal:
+                f.write('#include <{0}>\n'.format(elem))
+            f.write("\n#endif\n")
+
+    def update(self):
+        """
+                1 gather new state
+                2 generate file
+                3 write buffer
+                4 some test (make, src/test...)
+        """
+        self.incLocal = set()
+        self.incSys = set()
         self.checkSource('./src')
-#        self.makeHeader()
+        print "loc : ", self.incLocal
+        print "sys : ", self.incSys
+        self.makeHeader()
 #        self.makeMakefile()
 
     def __init__(self):
         self.name = pName
         self.src = []
-        self.inc = []
+        self.incLocal = set()
+        self.incSys = set()
         os.system('cp -r ~/config/oblovim/project/script .')
         os.system('mkdir src')
+        os.system('mkdir inc')
+        os.system('touch inc/' + self.name + '.h')
         os.system('cp -r ~/config/oblovim/project/template/main.c src/')
-        self.Update()
+        self.update()
         
 
 
--- oblovim/project/template/main.c
+++ oblovim/project/template/main.c
@@ -3,6 +3,9 @@
 #include <test2.h>
 #include "test3.h"
 
+#include "raytracer.h"
+#include <string.h>
+
 #include <test4.h>
 
 int	main(int ac, char **av)
