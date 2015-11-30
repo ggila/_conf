1097050085ed9a039a2b7752893868ea1c8c931e
ggila <ggilaber@student.42.fr>
2015-11-30 10:13:18 +0100

    setup project

--- oblovim/project/script/setupP.py
+++ oblovim/project/script/setupP.py
@@ -10,20 +10,20 @@ if len(sys.argv) != 2:
     exit(0)
 
 pName = sys.argv[1]
+includePattern = re.compile('\n#include [<"]([^">]+)[">]\n')
 
 print pName
 
-#├── .zshrc
 #├── script
 #│   ├── make_errno.py
 #│   ├── tree.py
 #|   ├── make_proto.py
 #|   ├── patron.vim
 #│   └── header.vim
-#└── Makefile
 #└── src
 #|   └── main.c
 #└── inc
+#|   └── $(pName).h
 
 class File:
     """
@@ -39,18 +39,40 @@ class Project:
     - src
     """
 
+
+
 #    def setUpDir(self):
 #        os.system('cp -r  ~/config/oblovim/project/template/newP .')
 
-    def addFile(self):
-        os.system('cp -r ~/config/oblovim/project/script .')
-        os.system('cp -r ~/config/oblovim/project/zshrc .zshrc')
-        os.system('mkdir src')
-        os.system('cp -r ~/config/oblovim/project/template/main.c src/')
+    def checkInclude(self, l):
+        for e in l: print e
+
+    def readCFile(self, path):
+        with open(path, 'r') as f:
+            text = "\n{0}".format(f.read())
+            self.checkInclude(re.findall(includePattern, text))
+
+    def checkSource(self, rep):
+        for f in os.listdir(rep):
+            if not os.path.isdir(rep + '/' + f) and f[0] != '.' and f[-2:] == '.c':
+                self.readCFile(rep + "/" + f)
+        for f in os.listdir(rep):
+            if os.path.isdir(rep + '/' + f) and f[0] != '.':
+                self.checkSource(rep + "/" + f)
+
+    def Update(self):
+        self.checkSource('./src')
+#        self.makeHeader()
+#        self.makeMakefile()
 
     def __init__(self):
         self.name = pName
-        self.addFile()
+        self.src = []
+        self.inc = []
+        os.system('cp -r ~/config/oblovim/project/script .')
+        os.system('mkdir src')
+        os.system('cp -r ~/config/oblovim/project/template/main.c src/')
+        self.Update()
         
 
 
--- oblovim/project/template/main.c
+++ oblovim/project/template/main.c
@@ -1,3 +1,10 @@
+#include "test1.h"
+
+#include <test2.h>
+#include "test3.h"
+
+#include <test4.h>
+
 int	main(int ac, char **av)
 {
 	(void)ac;
