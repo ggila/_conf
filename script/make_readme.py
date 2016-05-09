ad4d264aa515c145a689c59144a7502275927845
ggila <ggilaber@student.42.fr>
2016-05-09 18:57:24 +0200

    make_readme.py

--- script/make_readme.py
+++ script/make_readme.py
@@ -1,13 +1,12 @@
 # Scan through dir and setup README.md
 
-def readReadme(readme):
-    '''
-        if a README.md is met during os.walk(), this function is
+def readReadme(root):
+    ''' if a README.md is met during os.walk(), this function is
         used to get its description (4th line and followings ones)
     '''
-    with open(readme) as f:
+    with open(root + 'README') as f:
         i = 0
-        ret = '## Topic\n\n'
+        ret = 
         for line in f:
             if i == 4:
                 ret += line
@@ -15,7 +14,27 @@ def readReadme(readme):
             else: i += 1
         return readme
 
+def checkFile(source):
+    ''' open source file and return its description
+    '''
+    with open(source) as f:
+        if f.readline() != '# ggila\n': return '\n'
+        ret = ''
+        while True:
+            s = f.readline()
+            if s[0] != '#': return ret
+            ret += s[2:]
+
 for root, dirs, files in os.walk('.', topdown = False):
     if '.git' not in root:
         readme = '# {}\n\n'.format(root.split()[-1])
-        if 'README.md' in files: readme += readReadme(root + '/README.md')
+        if 'README.md' in files: readme += '## Topic\n\n' + readReadme(root)
+        for d in dirs:
+            if os.path.isfile(d+'/README.md'):
+                readme += '* **{0}**: {1}\n'.format(d, readReadme(root + d))
+            else: readme += '* **{}**\n'.format(d)    
+        for f in files:
+            readme += '* **{}**' + checkFile(root + f)
+        if ':' in readme:
+            with open(root + '/README.md') as f:
+                f.write(readme)
