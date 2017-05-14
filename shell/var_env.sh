58dd09ba50d5bd1f817491a20fed7b211b5a330e
ggila <ggilaber@student.42.fr>
2017-05-14 13:36:25 +0200

    clean

--- shell/var_env.sh
+++
@@ -1,9 +0,0 @@
-export _CONF_DIR="$HOME/_conf"
-export _GIT_DIR="$HOME/_git"
-export _VIM_DIR="$_CONF_DIR/oblovim"
-
-export PYTHONPATH="$HOME/insights:$HOME/insights/lbd-core"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
