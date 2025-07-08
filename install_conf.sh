27cda5ed5c72113b74c3d1eb908efa901d6d3496
ggila <ggilaber@student.42.fr>
2025-07-08 15:03:50 +0200

    cleaning

--- install_conf.sh
+++ install_conf.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ln -s ~/_conf/tmux.conf ~/.tmux.conf
 ln -s ~/_conf/zshrc ~/.zshrc
+ln -s ~/_conf/bashrc ~/.bashrc
+ln -s ~/_conf/tmux.conf ~/.tmux.conf
 ln -s ~/_conf/vimrc ~/.vimrc
 
-source ~/.zshrc
+if [ -n "$ZSH_VERSION" ]; then
+    source ~/.zshrc
+elif [ -n "$BASH_VERSION" ]; then
+    source ~/.bashrc
+fi
