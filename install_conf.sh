b2b1401d391023d0d935359d3d746f7e15565998
ggila <ggilaber@student.42.fr>
2025-07-20 23:43:54 +0200

    install_conf.sh checks

--- install_conf.sh
+++ install_conf.sh
@@ -1,12 +1,29 @@
+
 #!/bin/bash
 
-ln -s ~/_conf/zshrc ~/.zshrc
-ln -s ~/_conf/bashrc ~/.bashrc
-ln -s ~/_conf/tmux.conf ~/.tmux.conf
-ln -s ~/_conf/vimrc ~/.vimrc
+CONF_DIR="$HOME/_conf"
+
+declare -A FILES=(
+  ["$CONF_DIR/zshrc"]="$HOME/.zshrc"
+  ["$CONF_DIR/bashrc"]="$HOME/.bashrc"
+  ["$CONF_DIR/tmux.conf"]="$HOME/.tmux.conf"
+  ["$CONF_DIR/vimrc"]="$HOME/.vimrc"
+)
+
+for SRC in "${!FILES[@]}"; do
+  DEST="${FILES[$SRC]}"
+  if [ -e "$DEST" ] || [ -L "$DEST" ]; then
+    echo "Skipping: $DEST already exists"
+  else
+    ln -s "$SRC" "$DEST"
+    echo "Linked: $SRC → $DEST"
+  fi
+done
 
 if [ -n "$ZSH_VERSION" ]; then
     source ~/.zshrc
+    echo "source ~/.zshrc"
 elif [ -n "$BASH_VERSION" ]; then
     source ~/.bashrc
+    echo "source ~/.bashrc"
 fi
